ae28f0fc7807e8741b948406ecbde81800165573
tarepan <tarepan5884@gmail.com>
2022-05-29 11:44:24 +0000

    Refactor encoder

--- s3prlvc/networks/encoder.py
+++ s3prlvc/networks/encoder.py
@@ -1,16 +1,14 @@
 """Network modules of Encoder"""
 
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Any, List, Optional
 
 from torch import nn, Tensor, tensor # pylint: disable=no-name-in-module
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from extorch import Conv1dEx # pyright: ignore [reportMissingTypeStubs]; bacause of extorch
-from omegaconf import MISSING
+from omegaconf import MISSING, SI
 
-# The follow section is related to Tacotron2
-# Reference: https://github.com/espnet/espnet/blob/master/espnet/nets/pytorch_backend/tacotron2
 
 def encoder_init(module: Any):
     """Initialize encoder parameters."""
@@ -19,77 +17,93 @@ def encoder_init(module: Any):
 
 
 @dataclass
+class ConfConv:
+    """
+    Configuration of Conv (Ex), directly unpackable.
+
+    Args:
+        in_channels - Channel size of input
+        out_channels - Channel size of output
+        kernel_size - Size of conv kernel
+        causal - Whether Conv1d is causal or not
+    """
+    in_channels: int = MISSING # conf.conv_dim_c
+    out_channels: int = MISSING # conf.conv_dim_c
+    kernel_size: int = MISSING # conf.conv_size_k
+    stride=1
+    bias=False
+    causal: bool = MISSING # conf.causal
+
+@dataclass
+class ConfRNN:
+    """
+    Configuration of nn.RNN/GRU/LSTM, directly unpackable.
+
+    Args:
+        input_size - Dimension size of RNN input
+        num_layers - The number of RNN layers
+        bidirectional - Whether RNN is bidirectional or not
+    """
+    input_size: int = MISSING
+    num_layers: int = MISSING
+    batch_first: bool = True
+    bidirectional: bool = MISSING
+
+@dataclass
 class ConfEncoder:
     """Configuration of Taco2AR Encoder
 
     Args:
         dim_i: Dimension of the inputs
-        causal: Whether Conv1d is causal or not
+        dim_h: Dimension of hidden layer, equal to o_segFC, i_conv, o_conv and i_rnn
         num_conv_layers: The number of conv layers
-        conv_size_k: Size of conv kernel
-        conv_dim_c: Dimension size of conv channels
         conv_batch_norm: Whether to use batch normalization for conv
-        conv_residual: Whether to use residual connection for conv
         conv_dropout_rate: Conv dropout rate
-        bidirectional: Whether RNN is bidirectional or not
-        num_rnn_layers: The number of RNN layers
+        conv_residual: Whether to use residual connection for conv
         dim_o: Dimension size of output, equal to RNN hidden size
     """
     dim_i: int = MISSING
-    causal: bool = MISSING
+    dim_h: int = MISSING
     num_conv_layers: int = MISSING
-    conv_dim_c: int = MISSING
-    conv_size_k: int = MISSING
+    conv: ConfConv = ConfConv(
+        in_channels=SI("${..dim_h}"),
+        out_channels=SI("${..dim_h}"),)
     conv_batch_norm: bool = MISSING
-    conv_residual: bool = MISSING
     conv_dropout_rate: float = MISSING
-    bidirectional: bool = MISSING
-    num_rnn_layers: int = MISSING
+    conv_residual: bool = MISSING
+    rnn: ConfRNN = ConfRNN(
+        input_size=SI("${..dim_h}"),)
     dim_o: int = MISSING
 
 class Taco2Encoder(nn.Module):
-    """Encoder module of Taco2AR.
+    """Taco2AR's Encoder network.
 
-    Model: segFC[-(Res(Conv1d[-BN]-ReLU-DO))xN][-LSTMxM]
+    Model: <series>-segFC-([Res](Conv1d[-BN]-ReLU-DO))xN-LSTMxM-<series>
     """
 
     def __init__(self, conf: ConfEncoder):
         super().__init__()
 
-        # segFC linear
-        self._seg_fc = nn.Linear(conf.dim_i, conf.conv_dim_c)
+        # SegFC: Linear
+        self._seg_fc = nn.Linear(conf.dim_i, conf.dim_h)
 
-        # convs: [(Conv1d[-BN]-ReLU-DO)xN]
+        # Convs: (Conv1d[-BN]-ReLU-DO)xN
         self._use_conv_residual = conf.conv_residual
         self._convs = nn.ModuleList()
         for _ in range(conf.num_conv_layers):
             layer: List[nn.Module] = []
-            layer += [Conv1dEx(
-                        conf.conv_dim_c,
-                        conf.conv_dim_c,
-                        conf.conv_size_k,
-                        stride=1,
-                        padding=(conf.conv_size_k - 1) // 2,
-                        bias=False,
-                        causal=conf.causal,
-            )]
-            layer += [nn.BatchNorm1d(conf.conv_dim_c)] if conf.conv_batch_norm else []
+            layer += [Conv1dEx(padding=(conf.conv.kernel_size - 1) // 2, **asdict(conf.conv))]
+            layer += [nn.BatchNorm1d(conf.conv.out_channels)] if conf.conv_batch_norm else []
             layer += [nn.ReLU()]
             layer += [nn.Dropout(conf.conv_dropout_rate)]
             self._convs += [nn.Sequential(*layer)]
-
-        # blstm: [N-LSTM]
-        if conf.num_rnn_layers > 0:
-            dim_lstm = conf.dim_o // 2 if conf.bidirectional else conf.dim_o
-            self.blstm = nn.LSTM(
-                conf.conv_dim_c, dim_lstm, conf.num_rnn_layers, batch_first=True, bidirectional=conf.bidirectional
-            )
-            print(f"Encoder LSTM: {'bidi' if conf.bidirectional else 'uni'}")
-        else:
-            self.blstm = None
-
-        # initialize
         self.apply(encoder_init)
+
+        # RNN: N-LSTM
+        self._use_rnn = conf.rnn.num_layers > 0
+        if self._use_rnn:
+            dim_lstm_h = conf.dim_o // 2 if conf.rnn.bidirectional else conf.dim_o
+            self._rnn = nn.LSTM(hidden_size=dim_lstm_h, **asdict(conf.rnn))
 
     # Typing of PyTorch forward API is poor.
     def forward(self, x_series: Tensor, ilens: Optional[Tensor]=None): # pyright: reportIncompatibleMethodOverride=false
@@ -98,7 +112,7 @@ class Taco2Encoder(nn.Module):
             x_series (Batch, T_max, Feature_o): padded acoustic feature sequence
         """
 
-        # segFC linear
+        # SegFC
         x_series = self._seg_fc(x_series).transpose(1, 2)
 
         # Conv
@@ -108,14 +122,16 @@ class Taco2Encoder(nn.Module):
             else:
                 x_series = conv_layer(x_series)
 
-        # LSTM
-        if self.blstm is None:
+        # Early return w/o LSTM
+        if self._use_rnn is False:
             return x_series.transpose(1, 2)
+
+        # RNN
         if not isinstance(ilens, Tensor):
             ilens = tensor(ilens)
         xs_pack_seq = pack_padded_sequence(x_series.transpose(1, 2), ilens.cpu(), batch_first=True)
-        self.blstm.flatten_parameters()
-        xs_pack_seq, _ = self.blstm(xs_pack_seq)  # (B, Lmax, C)
+        self._rnn.flatten_parameters()
+        xs_pack_seq, _ = self._rnn(xs_pack_seq)  # (B, Lmax, C)
         # Pack then Pad
         out_seq, hlens = pad_packed_sequence(xs_pack_seq, batch_first=True)
         return out_seq, hlens
