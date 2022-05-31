ee99aa179f8cfc7849f04e207c2da781bef3cd91
tarepan <tarepan5884@gmail.com>
2022-05-31 15:36:31 +0000

    Add VCC20 train/val split

--- s3prlvc/data/corpora.py
+++ s3prlvc/data/corpora.py
@@ -55,15 +55,18 @@ def load_corpora(conf: ConfCorpora):
         spk_unseen = ["jvs095", "jvs096", "jvs098"]
         spk_seen_val = ["jvs094", "jvs099"]
         num_val_uttr = 10
-    # if corpus_name == "VCC20":
-    #     # Missing utterances in original code: E10001-E10050 (c.f. tarepan/s3prl#2)
-    #     self._sources = list(filter(lambda item_id: item_id.subtype == "eval_source", all_utterances))
-    #     self._targets = list(filter(lambda i: i.subtype == "train_target_task1", all_utterances))
+    elif conf.train.name == "VCC20":
+        # seen-speaker-seen-utterance & unseen-speaker-seen/unseen-utterance
+        uttrs = list(filter(lambda item_id: (item_id.subtype == "train_source") or (item_id.subtype == "train_target_task1"), uttrs))
+        spk_unseen = ["vcc20_TEF1", "vcc20_TEF2", "vcc20_TEM1", "vcc20_TEM2"]
+        # spk_senn = ["vcc20_SEF1", "vcc20_SEF2", "vcc20_SEM1", "vcc20_SEM2"]
+        spk_seen_val = ["vcc20_SEF1", "vcc20_SEM1"]
+        num_val_uttr = 10
     # elif corpus_name == "AdHoc":
     #     self._sources = list(filter(lambda item_id: item_id.subtype == "s", all_utterances))
     #     self._targets = list(filter(lambda item_id: item_id.subtype == "t", all_utterances))
     else:
-        raise Exception(f"Specified corpus do not supported in corpus split.")
+        raise Exception("Specified corpus do not supported in corpus split.")
     splits = split_spk_uttr(uttrs, spk_unseen, spk_seen_val, num_val_uttr)
     corpus_train_both = CorpusData(corpus, splits.seen_spk_seen_uttr)
     corpus_val_seen = CorpusData(corpus, splits.seen_spk_unseen_uttr)
